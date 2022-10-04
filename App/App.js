a24da565beccfd5903ee8322b0e9b40612912378
Dev1Dei <deividasbruzas333@gmail.com>
2022-10-04 03:04:44 +0300

    Panaktinėjau

--- App/App.js
+++ App/App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import SignInScreen from '../src/screens/SignInScreen';
+import CustomInput from '../src/components/CustomInput';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Bruh up App.js to start working on your app! Im straight from the underground</Text>
+      <SignInScreen />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  root:{
+    flex: 1,
+    backgroundColor: '#F9FBFC',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
