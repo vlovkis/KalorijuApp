766401f52cdcff683c57dbe00ab1c5de502d3827
Dev1Dei <deividasbruzas333@gmail.com>
2022-10-25 23:13:31 +0300

    pain in my ass holes

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {View, Text} from 'react-native'
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionic from 'react-native-vector-icons/Ionicons';
 import StatisticsScreen from '../StatisticScreen/Statistic';
 import AddFoodScreen from '../AddFoodScreen/AddFood';
@@ -12,7 +11,7 @@ import HomeScreen from '../HomeScreen/HomePage';
 
 const index = () => {
     const Tab = createBottomTabNavigator();
-    const Stack = createNativeStackNavigator();
+
     return (
         <NavigationContainer independent={true}>
             <Tab.Navigator
@@ -47,10 +46,6 @@ const index = () => {
                 <Tab.Screen name="Profile" component={ProfileScreen}/>
             </Tab.Navigator>
         </NavigationContainer>
-    
-
     )
 };
-
-
 export default index
